697b5f296a6833d16266a364a2d1e0c6de78b000
koss-sh <shilinskii22@gmail.com>
2023-05-12 16:53:44 +0300

    initial commit

---
+++ src/main/java/com/example/homework_calculator_tests/HomeworkCalculatorTestsApplication.java
@@ -0,0 +1,13 @@
+package com.example.homework_calculator_tests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkCalculatorTestsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeworkCalculatorTestsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/homework_calculator_tests/HomeworkCalculatorTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homework_calculator_tests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkCalculatorTestsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
